59b4edffcbfea9e8885b517b3f612ee523a5a103
sharikhollander-blip <sharikhollander@gmail.com>
2026-02-18 12:01:28 -0500

    Update 2-reader-header-centered.lua

--- 2-reader-header-centered.lua
+++ 2-reader-header-centered.lua
@@ -122,7 +122,7 @@ ReaderView.paintTo = function(self, bb, x, y)
 
     -- ===========================!!!!!!!!!!!!!!!=========================== -
     -- What you put here will show in the header:
-    local centered_header = string.format("%s %s %s", book_author, separator.en_dash, book_title)
+    local centered_header = string.format("%s %s %s", book_title)
     -- Look up "string.format" in Lua if you need help.
     -- ===========================!!!!!!!!!!!!!!!=========================== -
 
